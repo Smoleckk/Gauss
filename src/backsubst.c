2a92f2549cf24aad31bf29778b927cd7c8965a6d
Filip <wazplamisty123@gmail.com>
2021-01-21 21:16:15 +0100

    podstawienie w tyl

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,16 +5,17 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-			if (x->c!=1||x->r!=m->r||m->r!=m->c||b->c!=1||b->r!=m->r) return 2;
-			    for (int i=m->r-1;i>=0;i--) {
-				            double sum=0.0;
-					            for (int j=i+1;j<m->r;j++) {
-							                sum+=m->a[i][j]*x->a[j][0];
-									        }
-						            if (m->a[i][i]==0) return 1;
-							            x->a[i][0]=(b->a[i][0]-sum)/m->a[i][i];
-								        }
-			        return 0;
+	if (x->c!=1||x->r!=m->r||m->r!=m->c||b->c!=1||b->r!=m->r) return 2;
+	for (int i=m->r-1;i>=0;i--) {
+		double sum=0.0;
+		for (int j=i+1;j<m->r;j++) 
+			 sum+=m->a[i][j]*x->a[j][0];
+									     
+		if (m->a[i][i]==0) return 1;
+		
+		x->a[i][0]=(b->a[i][0]-sum)/m->a[i][i];
+	}							       
+	return 0;
 }
 
 
